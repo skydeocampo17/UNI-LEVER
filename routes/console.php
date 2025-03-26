a2f397a2738690a5b723c35f2f33cf66a6b95454
Chanix123-creator <lagudachristianmark69@gmail.com>
2025-03-26 09:52:38 -0500

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Lebron james
+
+// Tim Duncan
+
+// James Harden
+
+// Kobe Bryant
+
+// Michael Jordan
